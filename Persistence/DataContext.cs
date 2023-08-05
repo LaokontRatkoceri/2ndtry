46fa33ffdf81bb1a8d48533d9174fc9312333cd3
Laokont Ratkoceri <64806891+LaokontRatkoceri@users.noreply.github.com>
2023-08-05 17:19:03 +0200

    first commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+      //  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+    //        // You don't actually ever need to call this
+  //      }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+
+}
